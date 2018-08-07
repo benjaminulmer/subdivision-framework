e9ee20763eefd3268b53e7411bf94203345f1a8c
miamactavish <miamactavish@gmail.com>
2018-08-06 22:19:41 -0600

    Some prep for ray casting

--- skeleton/RayTracer.cpp
+++ skeleton/RayTracer.cpp
@@ -1,6 +1,16 @@
 #include "RayTracer.h"
 
+extern "C" void setTextureFilterMode(bool bLinearFilter);
+extern "C" void initCuda(void *h_volume, cudaExtent volumeSize);
+extern "C" void freeCudaBuffers();
+extern "C" void render_kernel(dim3 gridSize, dim3 blockSize, unsigned int *d_output, unsigned int imageW, unsigned int imageH,
+	float density, float brightness, float transferOffset, float transferScale);
+extern "C" void copyInvViewMatrix(float *invViewMatrix, size_t sizeofMatrix);
 
 RayTracer::RayTracer()
 {
+}
+
+void RayTracer::trace() {
+
 }
--- skeleton/RayTracer.h
+++ skeleton/RayTracer.h
@@ -1,28 +1,27 @@
 #pragma once
 // This is the class that links with NVIDIA CUDA to do ray casting on the gpu. 
 
-#include <helper_gl.h>
+#include <cudaSamples/helper_gl.h>
 
 // CUDA Runtime, Interop, and includes
-#include <cuda_runtime.h>
-#include <cuda_gl_interop.h>
-#include <cuda_profiler_api.h>
-#include <vector_types.h>
-#include <vector_functions.h>
-#include <driver_functions.h>
-
-// CUDA utilities
-#include <helper_cuda.h>
+#include <cuda/cuda_runtime.h>
+#include <cuda/cuda_gl_interop.h>
+#include <cuda/cuda_profiler_api.h>
+#include <cuda/vector_types.h>
+#include <cuda/vector_functions.h>
+#include <cuda/driver_functions.h>
 
 // Helper functions
-#include <helper_cuda.h>
-#include <helper_functions.h>
-#include <helper_timer.h>
+#include <cudaSamples/helper_cuda.h>
+#include <cudaSamples/helper_functions.h>
+#include <cudaSamples/helper_timer.h>
 
 class RayTracer
 {
 public:
 	RayTracer();
 	~RayTracer() {}
+
+	void trace();
 };
 
